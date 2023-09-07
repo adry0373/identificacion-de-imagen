85900b4dfd4b9e4a85c1d7327cd8f7dbe58cf0c7
adry0373 <100300747+adry0373@users.noreply.github.com>
2023-09-07 00:34:25 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function preload(){
+classifier = ml5.imageClassifier("DoodleNet")
+}
+
+function setup(){
+    canvas=createCanvas(500,500);
+    background("white");
+       //*****************************************
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+    
+}
+
+function clearCanvas(){
+    background("white");
+}
+
+function draw(){
+    strokeWeight(2);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY,mouseX,mouseY);
+    }
+
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult)
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("label").innerHTML="Etiqueta: "+results[0].label;
+
+        document.getElementById("confidence").innerHTML="Precisión: "+(results[0].confidence*100).toFixed(3)+"%"
+
+        utterThis=new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utterThis)
+
+    }
+
+}
